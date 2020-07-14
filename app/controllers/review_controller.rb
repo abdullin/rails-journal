974652d448325d1bc52565ab82f88df526d2cf72
Rinat Abdullin <rinat.abdullin@gmail.com>
2020-07-14 15:57:44 +0000

    Implement day view

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,11 +1,10 @@
 class ReviewController < ApplicationController
   def week
     today = Date.today
-
     @notes = Note.where(:created_at => today.beginning_of_week..today.end_of_week)
   end
 
-  def today
+  def day
     today = Date.today
     @notes = Note.where(:created_at => today.beginning_of_day..today.end_of_day)
     render "week"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'review/week'
-  get 'review/today'
+  get 'review/day'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
   #resources :days 
