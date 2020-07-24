b46ca62639721dd479c7b74dca6a7194c7b7bdf3
Rinat Abdullin <rinat.abdullin@gmail.com>
2020-07-24 07:36:49 +0000

    Remove "timeless?" legacy

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,12 +1,12 @@
 class ReviewController < ApplicationController
   def week
     today = Date.today
-    @notes = Note.where(:created_at => today.beginning_of_week..today.end_of_week)
+    @notes = Note.visible.where(:created_at => today.beginning_of_week..today.end_of_week)
   end
 
   def day
     today = Date.today
-    @notes = Note.where(:created_at => today.beginning_of_day..today.end_of_day)
+    @notes = Note.visible.where(:created_at => today.beginning_of_day..today.end_of_day)
     render "week"
   end
 
--- app/models/note.rb
+++ app/models/note.rb
@@ -2,14 +2,6 @@ class Note < ApplicationRecord
   belongs_to :journal
   has_rich_text :content
 
-  scope :visible, -> { where(archived_at:nil).where("created_at < ?", Date.today.beginning_of_day) }
+  scope :visible, -> { where(archived_at:nil).where("created_at < ?", DateTime.now) }
   scope :future, ->  { where(archived_at:nil).where("created_at > ?", DateTime.now) }
-
-
-
-
-  def timeless?
-    created_at.year == 9999
-    
-  end
 end
