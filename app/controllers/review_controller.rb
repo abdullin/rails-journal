eb863141b45bba30bdb285f0a5abe26acaa39736
Rinat Abdullin <rinat.abdullin@gmail.com>
2020-08-06 15:06:33 +0000

    Display category filter for the weekly review

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,11 +1,16 @@
 class ReviewController < ApplicationController
   def week
-    
+    @categories = load_categories
+
+
     @delta = params[:week].to_i || 0
     @date = Date.today + @delta.week
-    @notes = Note.visible
-      .where(:created_at => @date.beginning_of_week..@date.end_of_week)
-      .reverse
+
+    notes = Note.visible.within_week(@date)
+
+    @category  = params.fetch(:category, -1).to_i
+    notes = notes.with_category(@category) unless @category== -1
+    @notes = notes.reverse
   end
 
   def day
@@ -18,4 +23,11 @@ class ReviewController < ApplicationController
     @notes = Note.future.reverse
   end
 
+  private 
+  def load_categories
+    cat = Category.all.sort_by { | cat | cat.name.downcase }
+    cat << Category.new(name: "Other", id: 0)
+    cat << Category.new(name: "All", id: -1)
+    cat
+  end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,11 @@
 class Note < ApplicationRecord
+
   belongs_to :journal
   has_rich_text :content
 
-  scope :visible, -> { where(archived_at:nil).where("created_at < ?", DateTime.now) }
-  scope :future, ->  { where(archived_at:nil).where("created_at > ?", DateTime.now) }
+  scope :visible, -> { where(archived_at:nil).where("notes.created_at < ?", DateTime.now) }
+  scope :future, ->  { where(archived_at:nil).where("notes.created_at > ?", DateTime.now) }
+
+  scope :with_category, lambda { | id | joins(:journal).where("journals.category_id = ?", id) }
+  scope :within_week, lambda { | date | where(:created_at => date.beginning_of_week..date.end_of_week) }
 end
