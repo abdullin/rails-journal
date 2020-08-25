a2c496429ad0bd26c69babc62d7cf59f907de9ee
Rinat Abdullin <rinat.abdullin@gmail.com>
2020-08-25 12:29:41 +0000

    Mark journals with future as bold

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,12 @@ class HomeController < ApplicationController
 
   def index
     @groups = group_by_category Journal.not_cold
+    @future = get_future_journals
   end
 
   def cold
     @groups = group_by_category Journal.cold
+    @future = get_future_journals
     render 'index'
   end
 
@@ -14,5 +16,13 @@ class HomeController < ApplicationController
     journals.order(updated_at: :desc)
       .group_by(&:category)
       .sort_by { |cat, rest| cat.present? ? cat.name.downcase : "zzz"}
+  end
+
+  def get_future_journals
+    Note.future
+      .joins(:journal)
+      .select('journals.id as jid, min(notes.created_at) as most_recent')
+      .group('journals.id')
+      .map{ |c| [c.jid,c.most_recent]}.to_h 
   end
 end
