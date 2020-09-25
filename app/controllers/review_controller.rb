cd1476fa1f2bc48e3e5c7c026fcb5c84a95664fd
Rinat Abdullin <rinat.abdullin@gmail.com>
2020-09-25 11:14:57 +0000

    Display word and char count on the daily review

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -15,13 +15,26 @@ class ReviewController < ApplicationController
   def day
     @date = Date.today
     @notes = Note.visible.within_day(@date).chronologically
+
+    @words, @letters = count_words_and_letters(@notes)
   end
 
   private 
+
   def load_categories
     cat = Category.all.sort_by { | cat | cat.name.downcase }
     cat << Category.new(name: "Other", id: 0)
     cat << Category.new(name: "All", id: -1)
     cat
+  end
+
+
+
+  def count_words_and_letters(notes)
+    texts = notes.map { |n| n.content.to_plain_text }
+    words = texts.reduce(0) { |mem,x| mem+x.split.size }
+
+    letters = texts.reduce(0) { |mem,x| mem+x.size }
+    return words, letters
   end
 end
